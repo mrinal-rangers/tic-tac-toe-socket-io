81ce930e4a276154ecf1e2d3a8432bfdbb611037
Mrinal Deb <mrinal437a@gmail.com>
2024-03-20 19:07:20 +0530

    socket io connection done both from front end and backend part1 completed worksfor multiple games at the same time clean code , bugs fixed

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ const {Server} = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server);
 
+let arr= [];
+let playArray = [];
+
+io.on('connection',(socket)=>{
+
+    socket.on('find',(e)=>{
+        if(e.name != null){
+            arr.push(e.name);
+            if(arr.length>=2){
+                let p1obj ={
+                    p1name : arr[0],
+                    p1value:'X',
+                    p1move:""
+                }
+                let p2obj ={
+                    p2name : arr[1],
+                    p2value:'O',
+                    p2move:""
+                }
+                let obj = {p1:p1obj , p2:p2obj};
+                playArray.push(obj);
+                arr.splice(0,2);
+                io.emit('find',{allPlayers:playArray});
+            }
+        }
+    })
+
+})
+
 app.use(express.static(path.resolve("")));
 
 app.get('/',(req,res)=>{
