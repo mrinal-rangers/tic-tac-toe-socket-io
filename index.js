38aa99561585b8151283ad7bc0ad120c824cfeed
Mrinal Deb <mrinal437a@gmail.com>
2024-03-21 15:41:58 +0530

    check point 3 game logic remainging have emitted the part when someone clicks a button

--- index.js
+++ index.js
@@ -28,12 +28,27 @@ io.on('connection',(socket)=>{
                     p2value:'O',
                     p2move:""
                 }
-                let obj = {p1:p1obj , p2:p2obj};
+                let obj = {p1:p1obj , p2:p2obj,sum:1};
                 playArray.push(obj);
                 arr.splice(0,2);
                 io.emit('find',{allPlayers:playArray});
             }
         }
+    })
+
+    socket.on('playing',(e)=>{
+        if(e.value=== 'X'){
+            let objToChange = playArray.find((obj)=>obj.p1.p1name === e.name);
+            objToChange.p1.p1move = e.id ;
+            objToChange.sum ++ ;
+        }
+        if(e.value=== 'O'){
+            let objToChange = playArray.find((obj)=>obj.p2.p2name === e.name);
+            objToChange.p2.p2move = e.id ;
+            objToChange.sum ++ ;
+        }
+
+        io.emit('playing',{allPlayers:playArray});
     })
 
 })
